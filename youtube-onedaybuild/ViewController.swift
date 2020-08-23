dd081331d549e08998a3829b7a1a9bcd78fb54e0
Osvaldo Garcia <osgar@icloud.com>
2020-08-23 10:35:46 -0400

    initial xcode project creation

---
+++ youtube-onedaybuild/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-onedaybuild
+//
+//  Created by Osvaldo Garcia on 8/23/20.
+//  Copyright © 2020 Osvaldo Garcia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
