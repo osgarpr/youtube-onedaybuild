1843cef90108730263f528b14064b46c17481e80
Osvaldo Garcia <osgar@icloud.com>
2020-08-23 17:11:23 -0400

    adding ConstantsTemplate.swift

---
+++ youtube-onedaybuild/ConstantsTemplate.swift
@@ -0,0 +1,21 @@
+//
+//  ConstantsTemplate.swift
+//  youtube-onedaybuild
+//
+//  Created by Osvaldo Garcia on 8/23/20.
+//  Copyright © 2020 Osvaldo Garcia. All rights reserved.
+//
+
+/* add your own keys and uncomment the code */
+
+import Foundation
+
+/*
+ struct Constants {
+ 
+ static var API_KEY = "AIzaSyBUDBFh-TdQwByi8aIsvcM-OeQgJl_aRmw"
+ static var PLAYLIST_ID = "UUQ7QfNCfxCmCUs1WBFgpFPA"
+ static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+    
+}
+*/
